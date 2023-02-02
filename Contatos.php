474361ec2c4d781d3e3da9923d748714335c492b
Vini Callder <caldeiracaiovinicius@gmail.com>
2023-02-02 09:43:25 -0300

    fale conosco no centro

--- Contatos.php
+++ Contatos.php
@@ -1,14 +1,21 @@
+<?php ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Contatos</title>
 </head>
 <body>
     <div class="container">
         <div class="col-sm-6 col-md-4">
+<!--Titulo no Centro " Fale Conosco "-->
+
+<h1> Fale Conosco </h1>
+
+
             
         </div>
     </div>
